77b2f6765f48b4c46cce6b6d6a2f2ecc9401a509
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-10-25 10:44:56 +0200

    Update ejercicio2.c

--- Practica2.1/ejercicio2.c
+++ Practica2.1/ejercicio2.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<errno.h>
 #include<string.h>
+#include<string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int setuid(uid_t uid);
 
 int main() {
-
 setuid(0);
  
  	if(errno != 0) {
@@ -13,6 +16,7 @@ setuid(0);
 		printf("El codigo de error es: %i\n",errno);
 
  		return -1;
- 	}   
+ 	}  
+	
 	return 0;
 }
