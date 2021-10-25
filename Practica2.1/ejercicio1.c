b4a4487de619d375a77ef16bd99eea6d2bf5f393
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-10-25 10:44:30 +0200

    Update ejercicio1.c

--- Practica2.1/ejercicio1.c
+++ Practica2.1/ejercicio1.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 #include<errno.h>
 #include<string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int setuid(uid_t uid);
 
 int main() {
-
 setuid(0);
  
  	if(errno != 0) {
 		perror("Se produjo un error");
 		char *strerror(int errnum);
+		printf("El codigo de error es: %i\n",errno);
 
  		return -1;
- 	}   
+ 	}  
+	
 	return 0;
 }
+
+
 
