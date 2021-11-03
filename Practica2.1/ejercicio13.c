93932429df0968091d21de844adb008b306e9410
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-11-03 20:57:35 +0100

    Add files via upload

---
+++ Practica2.1/ejercicio13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<errno.h>
+#include<sys/time.h>
+
+#define N 1000000
+
+int main(int argv, char **argc){
+
+        struct timeval tv;
+
+        int rc = gettimeofday(&tv,NULL);
+ 
+        int ini = tv.tv_usec;
+        int act=0, i = 0;
+        for(i; i < N;i++){
+                int rc2 = gettimeofday(&tv,NULL); 
+                act = tv.tv_usec;       
+        }       
+        printf("El bucle ha tardado %i\n", act-ini);
+
+        return 0;
+}
