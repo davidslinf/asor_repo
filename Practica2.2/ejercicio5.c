1e6108e1e991a32f4095253bdad645f715b5b056
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-11-08 09:46:51 +0100

    Add files via upload

---
+++ Practica2.2/ejercicio5.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+
+int main(int argv, char **argc) {
+
+	int fd= open("Prueba.txt",O_CREAT|O_RDWR|O_TRUNC, 0645);
+	
+	if(fd==-1){
+		perror("Error al abrir el fichero");
+		return -1;
+	}
+
+	return 0;
+}
+
