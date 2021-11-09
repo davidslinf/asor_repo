fe73b894ff9e62ebde87d7dbfc113b858ee440be
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-11-09 12:38:27 +0100

    Add files via upload

---
+++ Practica2.2/ejercicio15.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+
+//Suponemos que el fichero se pasa por parametro
+int main(int argc, char * argv[]) {
+
+        if(argc == 2) {
+                int fd = open(argv[1], O_RDWR | O_CREAT , 0645);
+
+                if (fd == -1) {
+                        perror("Fallo al abrir el fichero \n");
+
+                        return -1;
+                }
+				
+				//Comprobamos el estado del cerrojo
+                int locked = lockf(fd, F_TEST, 0);
+
+                if(locked != -1){
+                        //Bloqueamos el cerrojo
+                        lockf(fd, F_LOCK, 0);
+                        //Escribimos la hora actual
+                        const time_t currTime = time(NULL);
+                        const  struct tm *currDate = localtime(&currTime);
+                        char * s = malloc(6);
+                        const char * format = "%H:%M"; 
+                        strftime(s, 6, format, currDate); 
+                        printf("%s\n", s);
+                        free(s);
+                        //Suspendemos la ejecucion durante 30 segundos
+                        sleep(3);
+                        close(fd);
+                        //Liberamos el cerrojo
+                        lockf(fd, F_ULOCK, 0);
+                }
+
+                else {
+                        perror("El lock ya esta bloqueado\n");
+                        return -1;
+                }
+        }
+
+        else {
+                perror("Introduce una ruta de fichero\n");
+                return -1;
+        }
+
+        return 0;
+}
+
+
+
