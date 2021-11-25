d78e6fcab152b3b548ea013f5fbf65e453198422
davidslinf <93117943+davidslinf@users.noreply.github.com>
2021-11-25 21:33:43 +0100

    Add files via upload

---
+++ Practica2.3/ejercicio7.c
@@ -0,0 +1,36 @@
+/*      El mensaje de finalizacion solo se mostrara en la version con system() ya que con execvp() se esta sustituyendo la imagen del proceso original por
+        la del comando que se está realizando.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv){
+
+        if(argc == 1) {
+                perror("Se necesita de un argumento\n");
+                return -1;
+        }
+
+        if(execvp(argv[1],&argv[2]) == -1) {
+                perror("Error en execvp()\n");
+                return -1;
+        }
+        printf("Comando execvp() ejecutado correctamente\n");
+/*
+        if(system(argv[1]) == -1) {
+                perror("Error en system()\n");
+                return -1;
+        }
+        printf("Comando system() ejecutado correctamente\n");
+*/
+        return 0;
+}
+
+
+
+
+
+
