53f2a8e68cb7adc0bb12f8c256d95c39a65d811b
davidslinf <93117943+davidslinf@users.noreply.github.com>
2022-11-14 11:11:55 +0100

    Update ejercicio11.c

--- Practica2.2/ejercicio11.c
+++ Practica2.2/ejercicio11.c
@@ -2,26 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
         if(argc == 2) {
+                if(argc == 2){
+                struc stat buf;
+                stat(argv[1], &buf);
+                if(buf.st_mode == ISREG){
+                        const char * Nombre =  argv[1];
+                        const char * TerminacionRig = ".hard";
+                        const char * TerminacionSim = ".sym";
 
-                const char * Nombre =  argv[1];
-                const char * TerminacionRig = ".hard";
-                const char * TerminacionSim = ".sym";
+                        char * NombreRigido = malloc(strlen(Nombre) + strlen(TerminacionRig) +1);
+                        char * NombreSimbolico = malloc(strlen(Nombre) + strlen(TerminacionSim) +1);
 
-                char * NombreRigido = malloc(strlen(Nombre) + strlen(TerminacionRig) +1);
-                char * NombreSimbolico = malloc(strlen(Nombre) + strlen(TerminacionSim) +1);
+                        strcat(strcpy(NombreRigido, Nombre), TerminacionRig);
+                        strcat(strcpy(NombreSimbolico, Nombre), TerminacionSim);
 
-                strcat(strcpy(NombreRigido, Nombre), TerminacionRig);
-                strcat(strcpy(NombreSimbolico, Nombre), TerminacionSim);
+                        link(Nombre, NombreRigido);
+                        symlink(Nombre, NombreSimbolico);
 
-                link(Nombre, NombreRigido);
-                symlink(Nombre, NombreSimbolico);
-
-                free(NombreRigido);
-                free(NombreSimbolico);
+                        free(NombreRigido);
+                        free(NombreSimbolico);
+                }
+                else{
+                        perror("No es un fichero regular\n");
 
         }
 
