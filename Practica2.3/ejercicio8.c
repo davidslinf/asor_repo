1ab8e6339a593bfedfe80ff9b91ab58faf039676
davidslinf <93117943+davidslinf@users.noreply.github.com>
2022-11-20 13:52:29 +0100

    Update ejercicio8.c

--- Practica2.3/ejercicio8.c
+++ Practica2.3/ejercicio8.c
@@ -1,51 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <sys/time.h>
-#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
-#define PATH_MAX 4096
+int main(int argc, char **argv){
 
-void MostrarAtributos() {
-
-        pid_t ProcesoID = getpid();
-        pid_t PadreID = getppid();
-        pid_t GrupoID = getpgid(0);
-        pid_t SesionID = getsid(0);
-
-        struct rlimit rlim;
-        getrlimit(RLIMIT_NOFILE, &rlim);
-        int MaxFicheros = rlim.rlim_max;
-
-        char* DirActual = malloc(PATH_MAX);
-        getcwd(DirActual, PATH_MAX);
-
-        printf("El PID es: %i\n", ProcesoID);
-        printf("El PPID es: %i\n", PadreID);
-        printf("El PGID es: %i\n", GrupoID);
-        printf("El SID es: %i\n", SesionID);
-        printf("El numero maximo de ficheros es: %i\n" , MaxFicheros);
-        if(DirActual == NULL) perror("No hay suficiente espacio en el buffer para la ruta\n");
-        else printf("El directorio de trabajo actual es: %s\n", DirActual);
-
-        free(DirActual);
-
-        return;
-}
-
-int main(int argc, char** argv) {
-
-        pid_t pid = fork();
-
-        if(pid == -1){ //Error
-                perror("Error en fork()\n");
+        if(argc == 1){
+                perror("Se necesitan argumentos\n");
                 return -1;
         }
-        else if(pid == 0){ //Hijo
+
+        pid_t pid = fork();
+ 	if(pid == -1){  //Error
+                perror("Error en el fork\n");
+                return -1;
+        }
+        if(pid == 0){   //Hijo
                 if(setsid() == -1){
-                       perror("Error al crear la sesion\n");
+                        perror("Error al crear nueva sesion\n");
                         return -1;
                 }
 
@@ -54,40 +30,30 @@ int main(int argc, char** argv) {
                         return -1;
                 }
 
-                MostrarAtributos();
-
-                int fd1 = open("/tmp/daemon.out", O_RDWR|O_CREAT, 0644);
-                int fd2 = open("/tmp/daemon.err", O_RDWR|O_CREAT, 0664);
-                int fd3 = open("/dev/null", O_RDWR|O_CREAT, 0644);
-
-                if(fd1 == -1 || fd2 == -1 || fd3 == -1) {
-                        perror("Error al abrir los ficheros\n");
+                int fd1 = open("/tmp/daemon.out", O_RDWR | O_CREAT, 0644);
+                int fd2 = open("/tmp/daemon.err", O_RDWR | O_CREAT, 0644);
+                int fd3 = open("/dev/null", O_RDWR | O_CREAT, 0644);
+		
+		if(fd1 == -1 || fd2 == -1 || fd3 == -1){
+                        perror("Error en el open\n");
                         return -1;
                 }
-                dup2(fd1,1);
-                dup2(fd2,2);
-                dup2(fd3,0);
 
-                if(argc == 1) {
-                        perror("Se necesita de un argumento\n");
+                dup2(fd1,1);    //Salida
+                dup2(fd2,2);    //Salida error
+                dup2(fd3,0);    //Entrada
+
+                if(execvp(argv[1], argv+1) == -1){
+                        perror("Error en execvp\n");
                         return -1;
                 }
-				
-		if(execvp(argv[1],&argv[2]) == -1) {
-                      	perror("Error en execvp()\n");
-                        return -1;
-                }
-                printf("Comando execvp() ejecutado correctamente\n");
         }
-        else { //Padre
-                //sleep(3)
-                MostrarAtributos();
+	 else{   //Padre
+                sleep(30);
+                int status;
+                waitpid(pid, &status, 0);
+                printf("Se acabo el zombie\n");
         }
-
         return 0;
 }
-
-
-
-
 
