699b0da46d703eba69b0ea78c33d4757c8861971
davidslinf <93117943+davidslinf@users.noreply.github.com>
2022-12-01 13:24:52 +0100

    Update ejercicio2.c

--- Practica2.4/ejercicio2.c
+++ Practica2.4/ejercicio2.c
@@ -1,68 +1,80 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv){
 
-        int h_p[2];
         int p_h[2];
+        int h_p[2];
 
-        if(pipe(h_p) == -1 || pipe(p_h) == 1) {
+        if(pipe(h_p) == -1 || pipe(p_h) == -1){
                 perror("Error en el pipe\n");
-                return -1;
+                return 0;
         }
 
         pid_t pid = fork();
 
-        if(pid == -1) { //Error
-                perror("Error en el fork()\n");
+        if(pid == -1){  //Error
+                perror("Error en el fork\n");
                 return -1;
         }
-        else if(pid == 0) { //Hijo
+if(pid == 0){   //Hijo
+                close(p_h[1]);
                 close(h_p[0]);
-                close(h_p[1]);
 
                 char mensRecibido[512];
                 int longMensaje = 0;
+                int i = 0;
 
-                while(i < 10) {
-                        longMensaje = read(p_h[0],mensRecibido, 512);
-                        mensaRecibido[longMensaje] = '\0';
-                        write(1,mensajeRecibido, longMensaje);
+                while(i < 10){
+
+                        longMensaje = read(p_h[0], mensRecibido, 511);
+                        mensRecibido[longMensaje] = '\0';
+                        write(1, mensRecibido, longMensaje);
 
                         sleep(1);
-						if(i < 9) write(h_p[1], "l", 2);
-                        else write(h_p[1], "q", 2);
+                        if(i < 9)
+                                write(h_p[1], "l", 2);
+                        else
+                                write(h_p[1], "q", 2);
                         i++;
                 }
-                close(h_p[1]);
+
                 close(p_h[0]);
+                close(h_p[1]);
+
         }
-        else { //Padre
-                close(h_p[1]);
+	else{   //Padre
                 close(p_h[0]);
+                close(h_p[1]);
 
                 char mensaje[512];
                 int longMensaje;
                 char mensRecibido[2];
 
-                while(strcmp(mensRecibido, "q") != 0) {
-                        longMensaje = read(0, mensaje, 512);
-                        mensaje[longMensaje] = '\0';
-						 write(p_h[1], mensaje, longMensaje);
+                while(strcmp(mensRecibido, "q") != 0){
 
-                        read(h_p[0], mensRecibido, sizeof(mensRecibido));
-                        printf("Se ha recibido el caracter %s del hijo\n", mensRecibido);
+                        longMensaje = read(0, mensaje, 511);
+                        mensaje[512] = '\0';
+
+                        write(p_h[1], mensaje, longMensaje);
+
+                        read(h_p[0], mensRecibido, 2);
+                        printf("[PADRE]Se ha recibido el caracter %s del hijo\n", mensRecibido);
+
                 }
-                wait(NULL);
-                close(h_p[0]);
                 close(p_h[1]);
-                printf("El padre ha acabado\n");
-        }
+                close(h_p[0]);
 
-        return 0;
+                int status;
+                waitpid(pid, &status, 0);
+        }
+	 return 0;
 }
+
 
