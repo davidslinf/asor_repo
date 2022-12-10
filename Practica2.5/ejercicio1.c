49c0dbf472d3319ff886239f4c805deff942c393
davidslinf <93117943+davidslinf@users.noreply.github.com>
2022-12-10 14:24:31 +0100

    Update ejercicio1.c

--- Practica2.5/ejercicio1.c
+++ Practica2.5/ejercicio1.c
@@ -9,9 +9,6 @@ int main(int argc, char ** argv){
 
         struct addrinfo hints;
         struct addrinfo *result, *i;
-        char host[NI_MAXHOST];
-        char serv[NI_MAXSERV];
-
 
         memset(&hints, 0, sizeof(struct addrinfo));
         hints.ai_flags = AI_PASSIVE;
@@ -19,20 +16,20 @@ int main(int argc, char ** argv){
         hints.ai_socktype = SOCK_STREAM;
         int rc = getaddrinfo(argv[1], argv[2], &hints, &result);
         if(rc == -1){
-                perror("Error en el addrinfo: %s\n", gai_strerror(rc));
+                printf("Error en el addrinfo: %s\n", gai_strerror(rc));
                 return -1;
         }
 
         for(i = result, i != 0, i = i->ai_next){
-                int rb = getnameinfo(i->ai_addr, i->ai_addrlen, host, NI_MAXHOST, serv, NI_MAXSERV,
-                        NI_NUMERICHOST | NI_NUMERICSERV);
+                char host[NI_MAXHOST];
+                int rb = getnameinfo(i->ai_addr, i->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
                 if(rb == -1){
                         perror("Error en el getnameinfo\n");
                         return -1;
                 }
+                 printf("%s\t%i\t%i\n", host, i->ai_family, i->ai_socktype);
         }
-         printf("HOST: %s, SERV: %s\n", host, serv);
-
+        
         freeaddrinfo(result);
 
         return 0;
