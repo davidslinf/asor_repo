24bd5944256fdc02edef4fa724d154fb307bbc66
davidslinf <93117943+davidslinf@users.noreply.github.com>
2022-12-12 12:03:48 +0100

    Update ejercicio3.c

--- Practica2.5/ejercicio3.c
+++ Practica2.5/ejercicio3.c
@@ -1,43 +1,54 @@
-#include <sys/socket.h>
-#include <netdb.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char **argv){
 
- struct addrinfo hints;
- memset(&hints, 0, sizeof(struct addrinfo));
- hints.ai_family = AF_UNSPEC;
- hints.ai_socktype = SOCK_DGRAM;
+        struct addrinfo hints;
+        struct addrinfo *res;
 
- struct addrinfo *res;
- int rc, sd;
+        if(argc < 4){
+                perror("Faltan argumentos\n");
+                return -1;
+        }
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_flags = AI_PASSIVE;
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_DGRAM;
 
- char buf[257];
+        int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+        if(rc == -1){
+                printf("Error en el getaddrinfo: %s\n", gai_strerror(rc));
+                return -1;
+        }
 
- if(argc < 4) {
-   perror("No hay suficientes argumentos\n");
-   return -1;
- }
+        int sd = socket(res->ai_family, res->ai_socktype, 0);
+        if(sd == -1){
+                perror("Error en el socket\n");
+                return -1;
+        }
+         ssize_t bytesEnv = sendto(sd, argv[3], strlen(argv[3]) + 1, 0, res->ai_addr, res->ai_addrlen);
+        if(bytesEnv == -1){
+                perror("Error en el sendto\n");
+                return -1;
+        }
 
- rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+        char buf[256];
+        ssize_t bytesRec = recvfrom(sd, buf, 256, 0, res->ai_addr, &(res->ai_addrlen));
+        if(bytesRec == -1){
+                perror("Error en el recvfrom\n");
+                return -1;
+        }
+        buf[bytesRec] = '\0';
 
- sd = socket(res->ai_family, res->ai_socktype, 0);
+        printf("%s\n", buf);
 
- sendto(sd, argv[3], strlen(argv[3]), 0, res->ai_addr, res->ai_addrlen);
+        freeaddrinfo(res);
 
- ssize_t size  = recvfrom(sd, buf, 256, 0, res->ai_addr, &(res->ai_addrlen));
- buf[257] = '\0';
+        close(sd);
 
- printf("%s\n", buf);
-
- freeaddrinfo(res);
-
- close(sd);
-
- return 0;
+        return 0;
 }
